74501552930959c74f22fbbe6c565516cd7fa689
晓楼 <271263823@qq.com>
2018-12-17 13:54:45 +0800

    web标题统一

--- resources/views/User/Commons/layout.blade.php
+++ resources/views/User/Commons/layout.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>威付宝 - [商户后台]</title>
+    <title>{{ config('app.name') }} - [商户后台]</title>
 {{--<title>{{ config('app.name', 'Laravel') }}</title>--}}
 <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
