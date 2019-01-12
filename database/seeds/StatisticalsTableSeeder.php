aa23c4883c67ac1930b3de424d6e0fe1481beec6
晓楼 <271263823@qq.com>
2019-01-12 15:05:27 +0800

    添加商户数据填充

---
+++ database/seeds/StatisticalsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class StatisticalsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $now = Carbon::now()->toDateTimeString();
+        DB::table('statisticals')->insert([
+            'user_id'  => '1',
+            'created_at'=> $now,
+            'updated_at'=> $now
+        ]);
+        DB::table('statisticals')->insert([
+            'user_id'  => 2,
+            'agent_id'  => 1,
+            'created_at'=> $now,
+            'updated_at'=> $now
+        ]);
+    }
+}
