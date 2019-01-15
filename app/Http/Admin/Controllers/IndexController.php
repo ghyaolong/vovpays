0de04c32690c9f843fff5c8fe94bbdebc5a05439
晓楼 <271263823@qq.com>
2019-01-15 16:14:52 +0800

    添加缓存，使用redis

--- app/Http/Admin/Controllers/IndexController.php
+++ app/Http/Admin/Controllers/IndexController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Admin\Controllers;
 
 use App\Services\OrderDayCountService;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
@@ -10,12 +12,16 @@ class IndexController extends Controller
     public function __construct(OrderDayCountService $orderDayCountService)
     {
         $this->orderDayCountService = $orderDayCountService;
+        Cache::rememberForever('systems', function () {
+            return DB::table('systems')->select('name','value')->get();
+        });
+
     }
 
     public function index()
     {
         $title = '主页';
-        $description = '统计有10分钟延迟';
+        $description = '今日统计数据有10分钟延迟,想看实时的请使用订单查询';
         $order_day_count = json_encode(convert_arr_key($this->orderDayCountService->getSevenDaysCount(),'tm'));
         return view('Admin.Index.index', compact('title', 'description','order_day_count'));
     }
