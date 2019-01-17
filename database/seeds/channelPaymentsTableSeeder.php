310c6875d05d6df4427890b914987bf2b4c60926
晓楼 <271263823@qq.com>
2019-01-17 15:51:42 +0800

    添加支付方式数据填充

---
+++ database/seeds/channelPaymentsTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class channelPaymentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $now = Carbon::now()->toDateTimeString();
+        DB::table('channel_payments')->insert([
+            'channel_id'   => '1',
+            'paymentName'  => '支付宝',
+            'paymentCode'  => 'alipay',
+            'runRate'      => '0.03',
+            'costRate'     => 0,
+            'created_at'=> $now,
+            'updated_at'=> $now
+        ]);
+        DB::table('channel_payments')->insert([
+            'channel_id'   => '1',
+            'paymentName'  => '微信',
+            'paymentCode'  => 'wechat',
+            'runRate'      => '0.03',
+            'costRate'     => 0,
+            'created_at'=> $now,
+            'updated_at'=> $now
+        ]);
+        DB::table('channel_payments')->insert([
+            'channel_id'   => '1',
+            'paymentName'  => '转银行卡',
+            'paymentCode'  => 'alipay_bank',
+            'runRate'      => '0.03',
+            'costRate'     => 0,
+            'created_at'=> $now,
+            'updated_at'=> $now
+        ]);
+    }
+}
