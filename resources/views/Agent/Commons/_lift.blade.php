0670ecad3a4356dae458ad870fb0b1d435922303
ccb <2165354416@qq.com>
2019-01-18 16:45:17 +0800

    第三方上分

--- resources/views/Agent/Commons/_lift.blade.php
+++ resources/views/Agent/Commons/_lift.blade.php
@@ -15,11 +15,10 @@
             <li class="header">主导航</li>
             <li class="active treeview">
             <li><a href="{{route('agent.index')}}"><i class="fa fa-circle-o text-aqua"></i><span>主页</span></a></li>
-            <li><a href="{{route('agent.user')}}"><i class="fa fa-circle-o text-aqua"></i><span>商户管理</span></a></li>
+            {{--<li><a href="{{route('agent.user')}}"><i class="fa fa-circle-o text-aqua"></i><span>商户管理</span></a></li>--}}
             <li><a href="{{route('agent.order')}}"><i class="fa fa-circle-o text-aqua"></i><span>交易管理</span></a></li>
-            <li class="active treeview">
-                <a href="{{route('agent.clearing')}}"><i class="fa fa-circle-o  text-aqua"></i> <span>结算管理</span></a>
-            </li>
+            <li><a href="{{route('agent.clearing')}}"><i class="fa fa-circle-o  text-aqua"></i> <span>结算管理</span></a></li>
+
             <li class="active treeview">
             <li>
                 <a href="{{route('agent.bankCard')}}"><i class="fa fa-circle-o  text-aqua"></i> <span>银行卡信息</span></a>
