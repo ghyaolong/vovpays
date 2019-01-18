f0033c5454af4a509934f5cd6609fce6ea704f57
ccb <2165354416@qq.com>
2019-01-18 15:05:57 +0800

    今日统计

--- routes/court.php
+++ routes/court.php
@@ -21,21 +21,23 @@ Route::group([], function ($router) {
         Route::get('order', 'OrderController@index')->name('court.order');
         Route::get('order/{id}/show', 'OrderController@show')->name('court.show');
 
+        Route::get('user','UserController@index')->name('user.index');
+
         //账号管理
-        Route::get('account/{type}','AccountPhoneController@index')->name('court.account');
-        Route::post('account','AccountPhoneController@store')->name('court.accountAdd');
-        Route::post('account/saveStatus','AccountPhoneController@saveStatus')->name('court.accountStatus');
-        Route::get('account/{id}/edit','AccountPhoneController@edit')->name('court.accountEdit');
-        Route::delete('account','AccountPhoneController@destroy')->name('court.accountDel');
-        Route::post('account/check','AccountPhoneController@checkUnique')->name('court.check');
+        Route::get('account/{type}', 'AccountPhoneController@index')->name('court.account');
+        Route::post('account', 'AccountPhoneController@store')->name('court.accountAdd');
+        Route::post('account/saveStatus', 'AccountPhoneController@saveStatus')->name('court.accountStatus');
+        Route::get('account/{id}/edit', 'AccountPhoneController@edit')->name('court.accountEdit');
+        Route::delete('account', 'AccountPhoneController@destroy')->name('court.accountDel');
+        Route::post('account/check', 'AccountPhoneController@checkUnique')->name('court.check');
 
         //银行卡账号
-        Route::post('accountBank','AccountBankCardsController@store')->name('court.accountBankAdd');
-        Route::get('accountBank','AccountBankCardsController@index')->name('court.accountBank');
-        Route::get('accountBank/{id}/edit','AccountBankCardsController@edit')->name('court.accountBankEdit');
-        Route::post('accountBank/saveStatus','AccountBankCardsController@saveStatus')->name('court.accountBankStatus');
-        Route::delete('accountBank','AccountBankCardsController@destroy')->name('court.accountBankDel');
-        Route::post('accountBank/checkBank','AccountBankCardsController@checkUnique')->name('court.checkBank');
+        Route::post('accountBank', 'AccountBankCardsController@store')->name('court.accountBankAdd');
+        Route::get('accountBank', 'AccountBankCardsController@index')->name('court.accountBank');
+        Route::get('accountBank/{id}/edit', 'AccountBankCardsController@edit')->name('court.accountBankEdit');
+        Route::post('accountBank/saveStatus', 'AccountBankCardsController@saveStatus')->name('court.accountBankStatus');
+        Route::delete('accountBank', 'AccountBankCardsController@destroy')->name('court.accountBankDel');
+        Route::post('accountBank/checkBank', 'AccountBankCardsController@checkUnique')->name('court.checkBank');
     });
 
 });
