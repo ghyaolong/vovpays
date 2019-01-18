f027d27c8eaeaf2526aede4b8e1f315c87043f8e
晓楼 <271263823@qq.com>
2019-01-18 16:08:01 +0800

    bug修改

--- resources/views/Admin/Commons/_footer.blade.php
+++ resources/views/Admin/Commons/_footer.blade.php
@@ -1,8 +1,7 @@
-<!-- 底部 -->
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 2.4.0
+        <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-    reserved.
+    <strong>Copyright &copy; 2019</strong>
+
 </footer>
