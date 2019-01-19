f0cf81e893fea696acdc7bac751efcf5f81224bd
晓楼 <271263823@qq.com>
2019-01-19 14:37:15 +0800

    修改

--- database/seeds/ChannelPaymentsTableSeeder.php
+++ database/seeds/ChannelPaymentsTableSeeder.php
@@ -23,24 +23,6 @@ class ChannelPaymentsTableSeeder extends Seeder
             'costRate'     => 0,
             'created_at'=> $now,
             'updated_at'=> $now
-            ],
-            [
-                'channel_id'   => '1',
-                'paymentName'  => '微信',
-                'paymentCode'  => 'wechat',
-                'runRate'      => '0.03',
-                'costRate'     => 0,
-                'created_at'=> $now,
-                'updated_at'=> $now
-            ],
-            [
-                'channel_id'   => '1',
-                'paymentName'  => '转银行卡',
-                'paymentCode'  => 'alipay_bank',
-                'runRate'      => '0.03',
-                'costRate'     => 0,
-                'created_at'=> $now,
-                'updated_at'=> $now
             ]
         ]);
     }
