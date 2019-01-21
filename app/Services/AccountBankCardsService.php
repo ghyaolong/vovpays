3526b5e0a1339a18e54a6b2e9065691ab11a7297
晓楼 <271263823@qq.com>
2019-01-21 22:13:43 +0800

    bug修改

--- app/Services/AccountBankCardsService.php
+++ app/Services/AccountBankCardsService.php
@@ -57,10 +57,11 @@ class AccountBankCardsService
     public function update(int $id, int $uid, array $data)
     {
         $data = array_except($data, ['_token']);
-        $bank = explode(",", $data['bank_name']);
-        $data['bank_name'] = $bank[0];
-        $data['bank_mark'] = $bank[1];
-
+        if(isset( $data['bank_name'])){
+            $bank = explode(",", $data['bank_name']);
+            $data['bank_name'] = $bank[0];
+            $data['bank_mark'] = $bank[1];
+        }
         return $this->accountBankCardsRepository->update($id, $uid, $data);
     }
 
