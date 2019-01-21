3b4432764ca2b8c708baa484b94c5c124f5e5d7f
wangchao <528776151@qq.com>
2019-01-21 01:58:40 -0800

    修改api页面显示

--- resources/views/User/Api/api.blade.php
+++ resources/views/User/Api/api.blade.php
@@ -4,33 +4,43 @@
     <link rel="stylesheet" href="{{ asset('plugins/bootstrap-switch/bootstrap-switch.min.css') }}">
 @endsection
 @section('content')
-    <div class="row" style="margin: 10px 100px">
-        {{--商户费率--}}
-        <div class="" style="margin: 50px;width: 370px;height: 479px;background:#ffffff;float: left">
-            <p style="font-size: 18px;margin: 15px;">通道费率</p>
-            <div class="row" style="text-align: center">
-                {{-- <img src="{{ asset('AdminLTE/dist/img/agent/t0.png') }}" alt="" style="margin-top: 20px"> --}}
+    <div class="row">
+     
+        {{--同步跳转--}}
+        <div class="col-md-12">
+            <div class="box box-primary box-solid">
+                <div class="box-header with-border">
+                    <h3 class="box-title">API列表</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse">
+                            <i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="box-body">
+                    <div class="box-body" style="padding:20px auto;font-size: 15px">
+                        <table class="table table-bordered" style="margin: 10px auto">
+                        <tr style="color: #999999;background: #f5f6f9">
+                                <th>编码</th>
+                                <th>通道名称</th>
+                                <th>通道费率</th>
+                                <th>通道状态</th>
+                        </tr>
+                        @foreach ($list as $value)
+                            <tr>
+                                    <td>{{ $value['paymentCode'] }}</td>
+                                    <td>{{ $value['paymentName'] }}</td>
+                                    <td>{{ $value['runRate'] * 100 }}%</td>
+                                    <td> @if ($value['status'] == 1)开通 @else 关闭 @endif </td>
+                            </tr>
+                        @endforeach
+                        </table>
+                        <br>
+                    </div>
+                </div>
             </div>
-            <table class="table" style="width: 340px;margin: 60px 10px 0 10px;font-size: 13px">
-                <tr style="color: #999999;background: #f5f6f9">
-                    <th>编码</th>
-                    <th>通道名称</th>
-                    <th>通道费率</th>
-                    <th>通道状态</th>
-                </tr>
-                @foreach ($list as $value)
-                    <tr>
-                        <td>{{ $value['paymentCode'] }}</td>
-                        <td>{{ $value['paymentName'] }}</td>
-                        <td>{{ $value['runRate'] * 100 }}%</td>
-                        <td> @if ($value['status'] == 1)开通 @else 关闭 @endif </td>
-                    </tr>
-                @endforeach
-
-            </table>
         </div>
-
-
     </div>
 
 @endsection
