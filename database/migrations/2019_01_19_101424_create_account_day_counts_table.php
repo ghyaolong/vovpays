2a5478008b4c878b71509a89e93467661307f19e
晓楼 <271263823@qq.com>
2019-01-21 22:31:38 +0800

    添加默认值

--- database/migrations/2019_01_19_101424_create_account_day_counts_table.php
+++ database/migrations/2019_01_19_101424_create_account_day_counts_table.php
@@ -17,7 +17,7 @@ class CreateAccountDayCountsTable extends Migration
         Schema::create('account_day_counts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('account')->comment('账号');
-            $table->unsignedInteger('user_id')->comment('账号拥有者id');
+            $table->unsignedInteger('user_id')->default(0)->comment('账号拥有者id');
             $table->decimal('account_amount',11,2)->default(0)->comment('账号单日成功交易金额');
             $table->unsignedInteger('account_order_count')->default(0)->comment('账号单日订单量');
             $table->unsignedInteger('account_order_suc_count')->default(0)->comment('account_order_suc_count');
