ecee836e6997cae0bf60f0ef5ebfe648e084fc8c
zh <1950796924@qq.com>
2019-01-30 11:35:55 +0800

    添加获取订单号失败  日志信息

--- app/Console/Commands/getOrderCallback.php
+++ app/Console/Commands/getOrderCallback.php
@@ -89,11 +89,12 @@ class getOrderCallback extends Command
             $data['money'] = $RegularGetBankInfo->getAmount($data['no'], $data['mark']);
             $cardNo = $RegularGetBankInfo->getCardNo($data['no'],$data['mark']);
             $key = $data['phoneid'].'_'.'alipay_bank2_'.$cardNo.'_'.$data['money'];
+            Log::info('orderCallback_getOrderno_fail:',[$key]);
             if(Redis::exists($key))
             {
                 $order_id = Redis::get($key);
             }else{
-                Log::debug('orderCallback_getOrderno_fail:',[$key]);
+                Log::info('orderCallback_getOrderno_fail:',[$key]);
                 return 1;
             }
         }else{
