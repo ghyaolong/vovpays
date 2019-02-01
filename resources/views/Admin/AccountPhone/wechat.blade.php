b22fd9e1c0b9727136ab708f20d9e38439e14f83
晓楼 <271263823@qq.com>
2019-02-01 21:55:42 +0800

    去掉固码烟瘴，隐藏固码

--- resources/views/Admin/AccountPhone/wechat.blade.php
+++ resources/views/Admin/AccountPhone/wechat.blade.php
@@ -104,17 +104,17 @@
                                        placeholder="请输入手机标识">
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="" class="col-xs-3 control-label">收款链接:</label>
-                            <div class="col-xs-9">
-                                <input type="text" class="form-control" name="qrcode" placeholder="请输入任意金额收款链接">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-xs-7" style="margin-top:-10px;">
-                                <a href="{{env('QRCOURSE')}}"  target="_blank"  style="margin-left: 150px">二维码链接生成方法</a>
-                            </div>
-                        </div>
+                        {{--<div class="form-group">--}}
+                            {{--<label for="" class="col-xs-3 control-label">任意金额码:</label>--}}
+                            {{--<div class="col-xs-9">--}}
+                                {{--<input type="text" class="form-control" name="qrcode" placeholder="请输入任意金额码内容">--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        {{--<div class="form-group">--}}
+                            {{--<div class="col-xs-7" style="margin-top:-10px;">--}}
+                                {{--<a href="{{env('QRCOURSE')}}"  target="_blank"  style="margin-left: 150px">获取任意金额码内容</a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
                         <div class="form-group">
                             <label for="" class="col-xs-3 control-label">限额:</label>
                             <div class="col-xs-9">
@@ -287,18 +287,6 @@
                                 },
                                 delay: 500,
                             }
-                        },
-                    },
-                    qrcode: {
-                        validators: {
-                            notEmpty: {
-                                message: '请输入任意金额收款链接!'
-                            },
-                            regexp: {
-                                regexp: /(https:)\/\/[\w\-_]+(\.[\w\-_]+)+([\S\-\.,@?^=%&:/~\+#]+)?/i,
-                                message: '请输入格式正确的收款链接'
-                            }
-
                         },
                     },
                     dayQuota: {
