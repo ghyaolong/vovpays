9f36e2ff4ecac324479a4b81472904b190922cf2
wangchao <528776151@qq.com>
2019-03-04 00:14:15 -0800

    添加商户通道池

---
+++ app/Services/AccountUpperService.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2019/1/15
+ * Time: 17:33
+ */
+
+namespace App\Services;
+
+use App\Repositories\AccountUpperRepository;
+
+class AccountUpperService
+{
+    protected $accountUpperRepository;
+
+    public function __construct(AccountUpperRepository $accountUpperRepository)
+    {
+        $this->accountUpperRepository = $accountUpperRepository;
+    }
+
+    /**
+     * 带分页查询
+     * @param array $data
+     * @param int $page
+     * @return mixed
+     */
+    public function getAllPage(int $page)
+    {
+        return $this->accountUpperRepository->getAllPage($page);
+    }
+
+    /**
+     * 添加
+     * @param array $data
+     * @return mixed
+     */
+    public function add(array $data)
+    {
+        $data = array_except($data, ['_token']);
+
+        return $this->accountUpperRepository->add($data);
+    }
+
+    /**
+     * 更新
+     * @param int $id
+     * @param array $data
+     * @return mixed
+     */
+    public function update(int $id, array $data)
+    {
+        $data = array_except($data, ['id','_token']);
+
+        return $this->accountUpperRepository->update($id, $data);
+    }
+
+    /**
+     * 根据id查询
+     * @param string $id
+     * @return mixed
+     */
+    public function findId(string $id)
+    {
+        return $this->accountUpperRepository->findId($id);
+    }
+
+    /**
+     * 伪删除
+     * @param int $id
+     * @return mixed
+     */
+    public function destroy(int $id)
+    {
+        return $this->accountUpperRepository->del($id);
+    }
+
+}
